4a0ecf4ea643642e1c05468c699252d223507393
vlondonoo <vlondonoo@gmail.com>
2020-05-02 11:48:42 -0500

    fix: changing url by default

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const  port = procces.env.PORT || 3000
+const  port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -20,7 +20,7 @@ hbs.registerPartials(partialsPath)
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
     res.render('index', {
         title: 'Weather',
         name: 'Veronica'
